d9323172611f713c41da232167a2d69b1c222b27
truptibankar <truptibankar12@gmail.com>
2020-08-31 19:21:29 +0530

    Enhancement in Master code

---
+++ src/test/java/JunitFramework/Testcasefile2.java
@@ -0,0 +1,26 @@
+package JunitFramework;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Testcasefile2 {
+	WebDriver driver;
+	String Base_url = "https:/amazon.in";
+	int implicit_wait = 20;
+	@Before
+	public void set_up()
+	{
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+	}
+	
+	@After
+	public void clean()
+	{
+		driver.quit();
+		
+	}
+
+}
